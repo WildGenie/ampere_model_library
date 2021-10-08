6cc6222315b85071703f568c17e2f27e93ca0a73
MarcelWilnicki <marcel@onspecta.com>
2021-10-08 11:17:01 +0200

    resnet 18 first commit

--- utils/tf.py
+++ utils/tf.py
@@ -140,3 +140,49 @@ class TFSavedModelRunner:
         perf = bench_utils.print_performance_metrics(
             self.__warm_up_run_latency, self.__total_inference_time, self.__times_invoked, batch_size)
         return perf
+
+
+class TFSavedModelKERAS:
+    """
+    A class providing facilities to run TensorFlow saved model (in SavedModel format).
+    """
+
+    def __init__(self):
+        """
+        A function initializing runner.
+        """
+        tf.config.threading.set_intra_op_parallelism_threads(bench_utils.get_intra_op_parallelism_threads())
+        tf.config.threading.set_inter_op_parallelism_threads(1)
+
+        self.model = None
+        self.__warm_up_run_latency = 0.0
+        self.__total_inference_time = 0.0
+        self.__times_invoked = 0
+
+    def run(self, input):
+        """
+        A function assigning values to input tensor, executing single pass over the network, measuring the time needed
+        and finally returning the output.
+
+        :return: dict, output dictionary with tensor names and corresponding output
+        """
+
+        start = time.time()
+        output = self.model(input)
+        finish = time.time()
+
+        self.__total_inference_time += finish - start
+        if self.__times_invoked == 0:
+            self.__warm_up_run_latency += finish - start
+        self.__times_invoked += 1
+        return output
+
+    def print_performance_metrics(self, batch_size):
+        """
+        A function printing performance metrics on runs executed by the runner so far.
+
+        :param batch_size: int, batch size - if batch size was varying over the runs an average should be supplied
+        """
+        perf = bench_utils.print_performance_metrics(
+            self.__warm_up_run_latency, self.__total_inference_time, self.__times_invoked, batch_size)
+        return perf
