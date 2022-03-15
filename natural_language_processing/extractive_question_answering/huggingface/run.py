7eca53b86a4ac59defaa21bb36f97dca132c5e2f
marcel.wilnicki <marcel.wilnicki@gmail.com>
2022-03-15 12:03:21 +0100

    wip

--- natural_language_processing/extractive_question_answering/huggingface/run.py
+++ natural_language_processing/extractive_question_answering/huggingface/run.py
@@ -73,6 +73,11 @@ def run_pytorch(model_name, batch_size, num_runs, timeout, squad_path, **kwargs)
 
         # print(type(torch.from_numpy(squad.get_input_ids_array())))
         # quit()
+
+        print(type(squad.get_input_ids_array()))
+        print(type(np.array(squad.get_input_ids_array(), dtype=np.int32)))
+        print(type(torch.from_numpy(np.array(squad.get_input_ids_array(), dtype=np.int32))))
+        quit()
         output = pytorch_runner.run(torch.from_numpy(np.array(squad.get_input_ids_array(), dtype=np.int32)))
 
         for i in range(batch_size):
