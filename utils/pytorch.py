7a8909676c1baeb49d54ab0f86067a7a2332cea9
marcel.wilnicki <marcel.wilnicki@gmail.com>
2022-03-15 11:44:43 +0100

    wip

--- utils/pytorch.py
+++ utils/pytorch.py
@@ -46,7 +46,7 @@ class PyTorchRunner:
                 finish = time.time()
             else:
                 start = time.time()
-                output = model.generate(input)
+                output = model.generate(torch.from_numpy(input[0]))
                 finish = time.time()
 
             self.__total_inference_time += finish - start
