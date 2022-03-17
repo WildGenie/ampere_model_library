c1f691dede656a81bf886eefa0b2250c13e9f066
marcel.wilnicki <marcel.wilnicki@gmail.com>
2022-03-17 10:32:05 +0100

    wip

--- natural_language_processing/extractive_question_answering/huggingface/run.py
+++ natural_language_processing/extractive_question_answering/huggingface/run.py
@@ -82,6 +82,7 @@ def run_pytorch(model_name, batch_size, num_runs, timeout, squad_path, **kwargs)
         print('*' * 50)
         print(output.start_logits)
         print('*' * 50)
+        print(np.argmax(output.start_logits))
         quit()
 
         for i in range(batch_size):
