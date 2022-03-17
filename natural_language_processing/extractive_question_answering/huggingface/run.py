b47de0a26957f0a26660e4946fd9231ef70eb8f0
marcel.wilnicki <marcel.wilnicki@gmail.com>
2022-03-17 12:28:35 +0100

    wip

--- natural_language_processing/extractive_question_answering/huggingface/run.py
+++ natural_language_processing/extractive_question_answering/huggingface/run.py
@@ -166,7 +166,7 @@ def run_pytorch(model_name, batch_size, num_runs, timeout, squad_path, **kwargs)
         #                  return_offsets_mapping=True, truncation="only_second", padding="max_length",
         #                  add_special_tokens=True)
 
-        return tokenizer(question, text, add_special_tokens=False, max_length=384)
+        return tokenizer(question, text, add_special_tokens=True, max_length=384, stride=128)
 
     def detokenize(answer):
         return tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(answer))
