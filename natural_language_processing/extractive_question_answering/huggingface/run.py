7c53d860287d560c76f286d8fc63e0faa50138c3
marcel.wilnicki <marcel.wilnicki@gmail.com>
2022-03-18 11:45:12 +0100

    wip

--- natural_language_processing/extractive_question_answering/huggingface/run.py
+++ natural_language_processing/extractive_question_answering/huggingface/run.py
@@ -70,12 +70,10 @@ def run_pytorch(model_name, batch_size, num_runs, timeout, squad_path, **kwargs)
 
     def run_single_pass(pytorch_runner, squad):
 
-        print(squad.get_input_ids_array())
-        print(type(squad.get_input_ids_array()))
-        print(squad.get_input_ids_array().shape)
-        quit()
+        my_list = squad.get_input_ids_array()
+        input = torch.Tensor(my_list).type(torch.int32)
 
-        output = pytorch_runner.run(torch.from_numpy(squad.get_input_ids_array()).type(torch.int32))
+        output = pytorch_runner.run(input)
 
         for i in range(batch_size):
 
